935873f3114580d6f8907a914c5be8b299f105d9
dekhil008 <dekhil.008@gmail.com>
2021-12-22 00:13:03 +0300

    finish get and post

--- server.js
+++ server.js
@@ -4,12 +4,32 @@ const app = express()
 const db =require("./db")
 const Todo =require("./todo")
 
+app.use(express.json())
 
 app.get("/",(req,res) => {
     res.json(" GET / is working")
 })
 app.get("/tasks",(req,res) => {
-    res.json(" GET / is working")
+    Todo.find({}, (err, data) => {
+        if (err){
+            console.log("ERRor: ",  err )
+        }else{
+            res.json(data);
+        }
+
+    })
+})
+
+app.post("/tasks",(req,res) => {
+   
+
+    Todo.create(req.body , (err, newTask) =>{
+        if (err) {
+            console.log("ERROR: ",err)
+        } else {
+            res.status(201).json(newTask)
+        }
+    })
 })
 
 
