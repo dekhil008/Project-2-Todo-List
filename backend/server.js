c75426ed6e66d047857f66922feaaa6ef562028d
dekhil008 <dekhil.008@gmail.com>
2021-12-27 20:30:14 +0300

    finish User login register

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const app = express()
 
 const db =require("./db")
 const Todo =require("./todo")
+const User =require("./User")
 
 app.use(express.json()) //to read body
 app.use(cors()) //to read body
@@ -73,9 +74,10 @@ app.post("/tasks",(req,res) => {
 })
 
 
-app.delete("/tasks/:id",(req,res) => {
-   console.log("35:",req.params.id)
 
+app.delete("/tasks/:id",(req,res) => {
+    console.log("35:",req.params.id)
+    
     Todo.deleteOne({_id: req.params.id}, (err, deleteObj) =>{
         if (err) {
             console.log("ERROR: ",err)
@@ -91,8 +93,8 @@ app.delete("/tasks/:id",(req,res) => {
 
 
 app.delete("/tasks",(req,res) => {
-   //console.log("35:",req.params.id)
-
+    //console.log("35:",req.params.id)
+    
     Todo.deleteOne({isCompleted:true}, (err, deleteObj) =>{
         if (err) {
             console.log("ERROR: ",err)
@@ -113,7 +115,7 @@ app.put("/tasks/:id",(req,res) => {
     Todo.updateOne(
         {_id: req.params.id},
         {title:req.body.newTitle},
-         (err, updateObj) =>{
+        (err, updateObj) =>{
         if (err) {
             console.log("ERROR: ",err)
             res.status(400).json(err)
@@ -126,15 +128,15 @@ app.put("/tasks/:id",(req,res) => {
         }
     })
 })
- 
+
 app.put("/tasks/:id/:isCompleted",(req,res) => {
-   //console.log("35:",req.params.id)
+    //console.log("35:",req.params.id)
 
     Todo.updateOne(
         {_id: req.params.id},
         {isCompleted:req.params.isCompleted},
-         (err, updateObj) =>{
-        if (err) {
+        (err, updateObj) =>{
+            if (err) {
             console.log("ERROR: ",err)
             res.status(400).json(err)
         } else {
@@ -147,6 +149,49 @@ app.put("/tasks/:id/:isCompleted",(req,res) => {
     })
 })
 
+
+app.post("/users/register",(req,res) => {
+
+     User.create(req.body , (err, newUser) =>{
+        if (err) {
+            console.log("ERROR: ",err)
+            res.status(400).json({message : "htis email already taken"})
+
+        } else {
+            //res.status(201).json(newUser)
+            res.status(201).json("create New user successfully")
+        }
+    })
+})
+
+app.post("/users/login", (req, res) => {
+    User.find({ email: req.body.email }, (err, arrUserFound) => {
+      if (err) {
+        console.log("ERROR: ", err);
+      } else {
+        // console.log(arrUserFound);
+        if (arrUserFound.length === 1) {
+          // we found the user
+          if (req.body.password === arrUserFound[0].password) {
+            // password correct
+            res.status(200).json({
+              message: "Login Successfully",
+              username: arrUserFound[0].username,
+            });
+          } else {
+            // password incorrect
+            res.status(400).json({
+              message: "Wrong password",
+            });
+          }
+        } else {
+          res.status(404).json({
+            message: "The email entered is not registered",
+          });
+        }
+      }
+    });
+  });
 
 
 app.listen(5000,()=> {
